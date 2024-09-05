012936bfcfe8631d4c4825af788f46f8f24c9c44
Ernesto Mederos <ernesto.mederosgarcia@dci.education>
2024-09-05 19:19:05 +0200

    form responsiveness worked out and downloading functionaiity added

--- src/components/sections/Download12Steps.jsx
+++ src/components/sections/Download12Steps.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import WorkflowGuide from "../../assets/docs/12STEPS.pdf";
 import DownArrow from "../elements/downArrow";
 import CenteredSection from "../layouts/centeredSection";
 import CenteredContainer from "../layouts/centeredContainer";
@@ -11,10 +12,10 @@ function Download12() {
     return (
         <CenteredSection>
             <CenteredContainer>
-                <a className="external-link" href="./portfolio.html">
-                    12STEPS-WEB-WORKFLOW FOR FREE!
+                <a className="external-link" href={WorkflowGuide} download="MEG4DEV-12-STEPS-WORKFLOW-GUIDE">
+                    CLICK TO DOWNLOAD!
                 </a>
-                {/* <img className="double-arrow-gold" src={DoubleArrowGold} alt="arrow" height="30px" /> */}
+                <img className="double-arrow-gold" src={DoubleArrowGold} alt="arrow" height="30px" />
             </CenteredContainer>
             <DownArrow />
         </CenteredSection>
